fe07dbf894ce3cdcc418c405a577b5dd7131edac
KylieALeBlanc <kal3nh@virginia.edu>
2018-08-07 15:18:37 -0400

    Unit test get_docs

---
+++ exemptions/test_docLinks.py
@@ -0,0 +1,28 @@
+from unittest import TestCase
+import gather
+
+
+class TestDocLinks(TestCase):
+
+    def test__get_docs(self):
+        url = 'https://tea.texas.gov/Texas_Schools/District_Initiatives/Districts_of_Innovation/'
+        docs_df = gather.DocLinks(url, print_interim = False).docs_df
+
+        # PDF
+        test_pdf_df = docs_df[docs_df.type == "pdf"]
+        self.assertIn('http://www.lfcisd.net/UserFiles/Servers/Server_758/File/LF/District%20of%20Innovation%20Plan%20pages.pdf',
+                      list(test_pdf_df[test_pdf_df.index == 'Los Fresnos CISD'].link))
+
+        # Word
+        test_word_df = docs_df[docs_df.type == "docx"]
+        self.assertIn(
+            'http://www.marionisd.net/upload/page/0020/DofI%20Plan.docx',
+            list(test_word_df[test_word_df.index == 'Marion ISD'].link))
+
+        # Google
+        test_google_df = docs_df[docs_df.type == "google"]
+        self.assertIn(
+            'https://drive.google.com/file/d/0ByTbbvh_1OW_c2pWakJiNENNZXM/preview',
+            list(test_google_df[test_google_df.index == 'Morton ISD'].link))
+
+
