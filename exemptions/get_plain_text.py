88d1ad522fdb08b41c9bfa5d5bb2c20dcb36f95b
KylieALeBlanc <kal3nh@virginia.edu>
2018-08-10 09:41:01 -0400

    Get text contents

--- exemptions/get_plain_text.py
+++ exemptions/get_plain_text.py
@@ -1,17 +1,25 @@
-import os
-import pandas as pd
 from urllib import request
-import tika
+
+import requests
 from tika import parser
-import pandas as pd
-import spacy
 import urllib.parse as parse
 from interruptingcow import timeout
 import ssl
 
+
 def get_link_contents(link):
+    """
+
+    :param link: link to a document (including an HTML doc),
+    :return: plain text of that document
+    """
     print('Current link: {}'.format(link), end='\r')
-    headers={'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) '
+                      'Chrome/50.0.2661.102 Safari/537.36'}
+    ctx = ssl.create_default_context()
+    ctx.check_hostname = False
+    ctx.verify_mode = ssl.CERT_NONE
 
     if 'drive.google.com' in link:
         try:
@@ -19,7 +27,7 @@ def get_link_contents(link):
         except Exception as e:
             print("Google Error:", e)
     try:
-        with timeout(45, exception = RuntimeError):
+        with timeout(45, exception=RuntimeError):
             req = request.Request(link, headers=headers)
             buff = request.urlopen(req, context=ctx)
             parsed = parser.from_buffer(buff)
@@ -30,10 +38,11 @@ def get_link_contents(link):
         try:
             resp = requests.get(link)
             parsed = parser.from_buffer(resp.content)
-            texts.append(parsed['content'])
+            return parsed['content']
         except:
             print('error:', e, 'link:', link)
-            texts.append('UNAVAILABLE')
+            return 'UNAVAILABLE'
+
 
 def convert_google_url(url):
     """
---
+++ exemptions/tests/test_get_link_contents.py
@@ -0,0 +1,37 @@
+from unittest import TestCase
+from exemptions import get_plain_text
+
+
+
+class TestGet_link_contents(TestCase):
+    def test_get_link_contents(self):
+        # simple case
+        url = 'http://www.hsisd.net/upload/page/0061/docs/HSISDs%20DOI%20Plan%20-Board%20Approval%20Copy.pdf'
+        test_text = 'Empowerment to innovate and think differently.'
+        text = get_plain_text.get_link_contents(url)
+        self.assertIn(test_text, text)
+
+        # error
+        url = 'htt#p://www.hsisd.net/upload/page/0061/docs/HSISDs%20DOI%20Plan%20-Board%20Approval%20Copy.pdf'
+        test_text = 'UNAVAILABLE'
+        text = get_plain_text.get_link_contents(url)
+        self.assertEqual(test_text, text)
+
+        # google doc
+        url = 'https://docs.google.com/document/d/1dg3O2cF88tqTy1OCTPbUv9S2_oPegKY2dnDVXpczoEU/pub'
+        test_text = 'To access these flexibilities, a school district must adopt an innovation plan'
+        text = get_plain_text.get_link_contents(url)
+        self.assertIn(test_text, text)
+
+        # word doc
+        url = 'https://s3.amazonaws.com/scschoolfiles/1286/campus_improvement_plan_2017-2018_2.docx'
+        test_text = 'By 2018-2019, 100% of Wheeler ISD students'
+        text = get_plain_text.get_link_contents(url)
+        self.assertIn(test_text, text)
+
+        # html
+        url = 'http://www.chisd.net/Page/9668'
+        test_text = '\n'
+        text = get_plain_text.get_link_contents(url)
+        self.assertIn(test_text, text)
+
