30d217ea4115786e6d94af982a21cafc2e5ca4af
KylieALeBlanc <kal3nh@virginia.edu>
2018-08-24 10:22:48 -0400

    Able to extract dates, but need to improve

--- exemptions/extract_dates.py
+++ exemptions/extract_dates.py
@@ -1,11 +1,13 @@
 import en_core_web_sm
 import re
+
 nlp = en_core_web_sm.load()
 
 
-def get_date_phrase_list(text, n_tokens_before = 8, n_tokens_after = 6):
+def get_date_phrase_list(text, n_tokens_before=8, n_tokens_after=6):
     doc = nlp(text)
-    dates_phrases = []
+    dates = []
+    phrases = []
     for token in doc:
         bad_match = re.findall('\.', token.text)
         if not bad_match:
@@ -14,12 +16,17 @@ def get_date_phrase_list(text, n_tokens_before = 8, n_tokens_after = 6):
             if date:
                 i = token.i
                 start = i - n_tokens_before
+                if start < 0:
+                    start = 0
                 end = i + (n_tokens_after + 1)
                 phrase = doc[start:end]
-                dates_phrases.append((str(token.text), phrase))
-    return dates_phrases
+                dates.append(str(token.text))
+                phrases.append(phrase)
+    phrases = [str(item) for item in phrases]
+    return dates, phrases
 
-def get_phrase_list(text, n_tokens_before = 8, n_tokens_after = 6):
+#TODO change number of tokens to sentence
+def get_phrase_list(text, n_tokens_before=8, n_tokens_after=6):
     doc = nlp(text)
     phrases = []
     for token in doc:
@@ -29,7 +36,21 @@ def get_phrase_list(text, n_tokens_before = 8, n_tokens_after = 6):
             if date:
                 i = token.i
                 start = i - n_tokens_before
+                if start < 0:
+                    start = 0
                 end = i + (n_tokens_after + 1)
                 phrase = doc[start:end]
                 phrases.append(phrase)
+                phrases = [str(item) for item in phrases]  # move to function
     return phrases
+
+
+def get_max_p_from_dict_list(list):
+    new_list = []
+    if list:
+        [new_list.append([float(value) for value in item.values()]) for item in list]
+        new_list = [item for sublist in new_list for item in sublist]
+        max_loc = new_list.index(max(new_list))
+    if not list:
+        max_loc = -999 #TODO change to missing
+    return max_loc
