dbc09c55ea165625fbad36d571996dba292c6e14
KylieALeBlanc <kal3nh@virginia.edu>
2018-09-07 12:46:44 -0400

    test

--- exemptions/start.py
+++ exemptions/start.py
@@ -1,4 +1,4 @@
 import os
-
+## TEST TEST TEST
 data_path = os.path.join('/Users/kylieleblancKylie/dofis/data', 'plans')
 code_path = os.path.join('/Users/kylieleblancKylie/dofis/code', 'exemptions')
