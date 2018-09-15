3d289e97a673df2ccb938e84bfdc3a0961c7b07c
KylieALeBlanc <kal3nh@virginia.edu>
2018-09-14 21:17:23 -0400

    initial commit of merge tea and exemptions

---
+++ clean/tests/test_resolve_unicode_problems.py
@@ -0,0 +1,12 @@
+from unittest import TestCase
+from clean.library import clean_exemptions
+import pandas as pd
+
+class TestResolve_unicode_problems(TestCase):
+    def test_resolve_unicode_problems(self):
+        test = 'Bronte\xa0ISD'
+        d = {'distname': [test, 'Abbott ISD']}
+        df = pd.DataFrame(data=d)
+        df = clean_exemptions.resolve_unicode_problems(df, 'distname')
+        result = df[df.distname.str.startswith('Bronte')]['distname'].values[0]
+        self.assertEqual('Bronte ISD', result)
