68dcf6e50c345d1281603c98382c579913072ffb
KylieALeBlanc <kal3nh@virginia.edu>
2018-10-06 15:22:59 -0400

    Makes Geography Table

---
+++ analysis/regulations.py
@@ -0,0 +1,23 @@
+reg_schedules = ['reg25_0811', 'reg25_081', 'reg25_0812', 'reg25_082']
+reg_class_size = ['reg25_112', 'reg25_113', 'reg25_111']
+reg_certification = ['reg21_003', 'reg21_053', 'reg21_057']
+reg_contracts = ['reg21_102', 'reg21_401', 'reg21_352', 'reg21_354']
+reg_behavior = ['reg25_092', 'reg37_0012', 'reg25_036']
+
+labels = {'reg25_0811':'25.0811 - Minimum First Day of Instruction',
+          'reg25_081': '25.081 - Minimum Minutes of Operation',
+          'reg25_0812': '25.0812 - Minimum Last Day of Instruction',
+          'reg25_082': '25.082 - Pledge of Allegiance and Minute of Silence',
+          'reg25_112':'25.112 - Class Size Maximum',
+          'reg25_113': '25.113 - Notice of Class Size',
+          'reg25_111': '25.111 - Maximum Student Teacher Ratio',
+          'reg21_003':'21.003 - Teacher Certification Required',
+          'reg21_053': '21.053 - Presentation of Teacher Certificates',
+          'reg21_057': '21.057 - Notice of Uncertified Teacher',
+          'reg21_102': '21.102 - Maximum Probationary Contract Length',
+          'reg21_401': '21.401 - Minimum Service Days Required for Teachers',
+          'reg21_352': '21.352 - Appraisal of Teachers',
+          'reg21_354': '21.354 - Principal Appriasal',
+          'reg25_092': '25.092 - Minimum Attendance for Class Credit',
+          'reg37_0012': '37.0012 - Designation of Campus Behavior Coordinator',
+          'reg25_036': '25.036 - Transfers'}
