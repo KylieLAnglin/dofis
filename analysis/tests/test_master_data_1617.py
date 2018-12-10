50a9a2a9c9aeb51ec60ea24004c45f0f3dda6a72
KylieALeBlanc <kal3nh@virginia.edu>
2018-12-09 22:06:30 -0500

    passes district-level unit tests

--- analysis/tests/test_master_data_1617.py
+++ analysis/tests/test_master_data_1617.py
@@ -31,18 +31,38 @@ class TestCampusDataIntegrity1617(unittest.TestCase):
         Number of students taken from
         https://tea.texas.gov/Student_Testing_and_Accountability/Testing/State_of_Texas_Assessments_of_Academic_Readiness_(STAAR)/STAAR_Statewide_Summary_Reports_2016-2017/
         Average scores taken from: https://tea.texas.gov/staar/rpt/sum/
+
+        # english statistics taken from state level data https://tea.texas.gov/Student_Testing_and_Accountability/Testing/State_of_Texas_Assessments_of_Academic_Readiness_(STAAR)/STAAR_Aggregate_Data_for_2016-2017/
+        wrong in pdf
         """
         ground_truth_testers_1617 = {'r_3rd_numtakers': 370790,
-                                     'r_5th_numtakers': 379532, 'm_5th_numtakers': 387610,
-                                     'r_8th_numtakers': 380566}
+                                     'r_5th_numtakers': 379532,
+                                     'r_8th_numtakers': 380566,
+                                     'eng1_numtakers': 479150} # note english should actually fail. state data = 479552
+
+        ground_truth_scores_1617 = {'r_3rd_avescore': 1440,
+                                    'r_5th_avescore': 1553,
+                                    'r_8th_avescore': 1676,
+                                    'eng1_avescore': 3922}
 
         data = pd.read_csv(os.path.join(data_path, 'clean', 'master_data.csv'), sep=",")
 
         for yr in ['yr1617']:
             data = data[data.year == yr]
-            for sub in ['r_3rd_numtakers', 'r_5th_numtakers','m_5th_numtakers', 'r_8th_numtakers']:
+
+            for sub in ground_truth_testers_1617:
                 numtakers = data[sub].sum()
-            self.assertTrue(numtakers - 50 <= ground_truth_testers_1617[sub] <= numtakers + 50)
+                print(sub, ' ', numtakers)
+                self.assertTrue(numtakers - 50 <= ground_truth_testers_1617[sub] <= numtakers + 50)
+
+            for num, sub in zip(ground_truth_testers_1617, ground_truth_scores_1617):
+                total = data[num].sum()
+                data['weight'] = data[num]/total
+                data['weighted'] = list(data[sub] * data['weight'])
+                test = data['weighted'].sum()
+                print(sub, test, 'score should be', ground_truth_scores_1617[sub])
+                self.assertTrue(test - 25 <= ground_truth_scores_1617[sub] <= test + 25)
+
 
 
 if __name__ == '__main__':
