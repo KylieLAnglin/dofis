e877e285a43ee7d87bb8427ad9ebd2ce204b057e
KylieLAnglin <kylie@anglin.com>
2020-04-17 19:16:20 -0400

    update stats in 01_district_eligibility_and_takeup

--- analysis/Who Needs Rules/01_district_eligibility_and_takeup.py
+++ analysis/Who Needs Rules/01_district_eligibility_and_takeup.py
@@ -1,15 +1,7 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
+# %%
 import sys
 sys.path.append("../")
-
-
 import os
-get_ipython().run_line_magic('matplotlib', 'inline')
 import matplotlib.mlab as mlab
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -17,76 +9,52 @@ import numpy as np
 import statsmodels.formula.api as smf
 from patsy import dmatrices
 from openpyxl import load_workbook
+from library import start
 
-data_path = start.data_path
-table_path = start.table_path
-
-
-# In[2]:
+import matplotlib
+plt.style.use('grayscale')
 
 
-data = pd.read_csv(os.path.join(data_path, 'clean', 'master_data_district.csv'),
-                  sep=",", low_memory= False)
+# %%
+
+data = pd.read_csv(os.path.join(start.data_path, 'clean', 'master_data_district.csv'), 
+sep=",", low_memory= False)
 data15 = data[data.year == 2015]
 data18 = data[data['year'] == 2018]
-data18.loc[:, 'eligible'] = np.where(((data18.rating_academic == 'D') | (data18.rating_academic == 'F')), False, True)
+data19 = data[data['year'] == 2019]
+
 # Ignore charters
-data18 = data18[data18.distischarter == "N"]
+data15 = data15[data15.distischarter == 0]
+data18 = data18[data18.distischarter == 0]
+data19 = data19[data19.distischarter == 0]
 
 
-# In[3]:
+# %%
+print('Number of traditional public school districts in Texas in 2019:')
+print(data19.district.nunique())
+print('What percent of traditional public school district were eligible in 2015?')
+print(data15.eligible.mean())
+print('And in 2017-18?')
+print(data18.eligible.mean())
+print('And in 2018-19?')
+print(data19.eligible.mean())
+
+print('How many tradtional public districts are DOIs as of March 2019?')
+print(data[data.doi == True].district.nunique())
+print('Only', data18[(data18.doi == False) & (data18.eligible == True)].district.nunique(), 'districts have opted out.')
+print('What percent of districts are DOIs as of March 2019?')
+print(data[data.doi == True].district.nunique()/data[data.distischarter == 0].district.nunique())
 
 
-len(data18)
-
-
-# In[4]:
-
-
-# Ineligible
-len(data18[(data18.eligible == False) & (data18.doi == False)])
-
-
-# In[5]:
-
-
-# Number opting out
-len(data18[((data18.eligible == True) & (data18.doi == False))])
-
-
-# In[6]:
-
-
-# DOIs
-len(data18[data18.doi == True])
-
-
-# In[7]:
-
-
-data18.doi.value_counts()
-
-
-# In[8]:
+# %%
 
 
 district_df = pd.DataFrame(data.groupby(['district']).agg({'doi_year': 'mean'}))
 district_counts = pd.DataFrame(district_df.doi_year.value_counts(sort = False))
 district_counts = district_counts.sort_index()
-district_counts
+print('Cumulative count of DOIs in each year:')
+print(district_counts.doi_year.cumsum())
 
-
-# In[9]:
-
-
-district_counts.doi_year.cumsum() 
-
-
-# In[11]:
-
-
-import matplotlib
-plt.style.use('grayscale')
 my_dpi=96
 plt.figure(figsize=(480/my_dpi, 480/my_dpi), dpi=my_dpi)
 
@@ -103,12 +71,6 @@ plt.ylim(0, 1022)
 txt="Notes: Statistics are as of March 2019. There are ten Districts of Innovation (with missing Innovation Plans) \n that are not included in the figure. As of 2019, there were 1022 traditional public school districts in Texas."
 plt.figtext(.5,-.01, txt, wrap=True, horizontalalignment='center', fontsize=8)
 
-plt.savefig(table_path + 'takeup.png', dpi = 600, bbox_inches="tight")
+plt.savefig(start.table_path + 'takeup.png', dpi = 600, bbox_inches="tight")
 plt.show()
-
-
-# In[ ]:
-
-
-
 
