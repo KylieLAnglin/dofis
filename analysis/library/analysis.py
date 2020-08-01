9c03a93d3e18a21f519824b514d1cd8f69dfdd64
KylieLAnglin <kylie@anglin.com>
2020-08-01 15:46:35 -0400

    add functions to analysis

--- analysis/library/analysis.py
+++ analysis/library/analysis.py
@@ -168,3 +168,49 @@ def two_means_by_exemptions(df, exemption, var_list):
     data['pvalue'] = pvalues_col
 
     return data
+
+
+def create_interactions(variable: str, data: pd.DataFrame):
+    data['treatpost_' + variable] = data['treatpost'] * data[variable]
+    data['yearpost_' + variable] = data['yearpost'] * data[variable]
+    data['yearpre_' + variable] = data['yearpre'] * data[variable]
+
+    for preyr in [5, 4, 3, 2]:
+        data[variable + '_pre' + str(preyr)] = data[variable] * data['pre' + str(preyr)]
+    for postyr in [1, 2, 3]:
+        data[variable + '_post' + str(postyr)] = data[variable] * data['post' + str(postyr)]
+    return data
+
+def create_gdid_model_w_interactions(variable: str):
+    # no need for variable effect alone because this is captured by
+    # entity effects
+    gdid_model = 'score_std ~ + 1 + treatpost + '
+    gdid_model = gdid_model + 'treatpost_' + variable + ' + '
+    gdid_model = gdid_model + 'C(test_by_year) + '
+    gdid_model = gdid_model + 'EntityEffects'
+    print(gdid_model)
+    return gdid_model
+
+
+def create_linear_model_w_interactions(variable: str):
+    linear_gdid_model = 'score_std ~ + 1 + treatpost + yearpost + yearpre + '
+    linear_gdid_model = linear_gdid_model + 'treatpost_' + variable + ' + '
+    linear_gdid_model = linear_gdid_model + 'yearpre_' + variable + ' + '
+    linear_gdid_model = linear_gdid_model + 'yearpost_' + variable + ' + '
+    linear_gdid_model = linear_gdid_model + ' + C(test_by_year) + '
+    linear_gdid_model = linear_gdid_model + 'EntityEffects'
+
+    return linear_gdid_model
+
+
+def create_event_model_w_interactions(variable: str):
+    event_study_model = 'score_std ~ + 1 + pre5 + pre4 + pre3 + pre2 + ' \
+        'post1 + post2 + post3'
+    for preyr in ["_pre5", "_pre4", "_pre3", "_pre2"]:
+        event_study_model = event_study_model + ' + ' + variable + preyr
+    for postyr in ["_post1", "_post2", "_post3"]:
+        event_study_model = event_study_model + ' + ' + variable + postyr
+    event_study_model = event_study_model + ' + C(test_by_year) + EntityEffects'
+    print(event_study_model)
+
+    return event_study_model
