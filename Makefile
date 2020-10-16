9bc24943b2d26195cd23a3c5209c9c5863744b9b
KylieLAnglin <kylie@anglin.com>
2020-10-16 09:58:01 -0400

    archive

--- Makefile
+++
@@ -1,2 +0,0 @@
-all:
-    +$(MAKE) -C /Users/kylie/dofis/src/merge_and_clean
--- run_tea_merge_clean.sh
+++
@@ -1,9 +0,0 @@
-
-cd "/Users/kylie/dofis/src/data_from_tea"
-python '/Users/kylie/dofis/src/data_from_tea/c01_clean_tea_data.py'
-python '/Users/kylie/dofis/src/data_from_tea/c02_append_years.py'
-python '/Users/kylie/dofis/src/data_from_tea/d01_clean_tea_data.py'
-python '/Users/kylie/dofis/src/data_from_tea/d02_append_years.py'
-
-cd "/Users/kylie/dofis/src/merge_and_clean"
-python '/Users/kylie/dofis/src/merge_and_clean/01_merge_tea_and_exemptions.py'
