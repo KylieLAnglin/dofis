cd71c787ce19498bb99a0fcf547a4a8b65788eea
KylieLAnglin <kylie@anglin.com>
2021-02-03 14:31:20 -0500

    defns change pre 2016, limit cert years for effect

--- analysis/X_effect_certification.py
+++ analysis/X_effect_certification.py
@@ -25,8 +25,8 @@ data_school = data_school[data_school.doi]
 df = pd.read_csv(
     os.path.join(start.data_path, "clean", "gdid_school.csv"), sep=",", low_memory=False
 )
-df = df[~df.teachers_secondary_math_teachers_with_cert.isnull()]
-df = df[df.post3 == 0]
+df = df[df.year.isin([2016, 2017, 2018, 2019])]
+# df = data_school.copy()
 
 # convert year to datetime
 df = df.reset_index()
@@ -36,12 +36,45 @@ df = df.set_index(["year", "campus"])
 # swap indexes
 df.index = df.index.swaplevel(0, 1)
 
+df["pre2"] = np.where((df.pre2 == 1) | (df.pre3 == 1), 1, 0)
 
-event_study_model = (
-    "teachers_secondary_math_teachers_with_cert ~ + 1 + pre5 + pre4 + pre3 + pre2 + "
-    "post1 + post2 + EntityEffects + TimeEffects"
-)
+EVENT_STUDY_MODEL = "~ + 1 + pre2 + post1 + post2 + post3 + EntityEffects + TimeEffects"
+# %% Effect Certification
+
+event_study_model = "teachers_certified" + EVENT_STUDY_MODEL
 
 mod = PanelOLS.from_formula(event_study_model, data=df)
 res = mod.fit(cov_type="clustered", clusters=df.district)
+print(res)
+
+# %% Effect Uncertified Teachers
+
+
+mod = PanelOLS.from_formula("teachers_uncertified" + EVENT_STUDY_MODEL, data=df)
+res = mod.fit(cov_type="clustered", clusters=df.district)
+print(res)
+
+# %% # %% Effect secondary math
+
+mod = PanelOLS.from_formula(
+    "teachers_secondary_math_teachers_with_cert" + EVENT_STUDY_MODEL, data=df
+)
+res = mod.fit(cov_type="clustered", clusters=df.district)
+print(res)
+
+
+# %% NUMBER of out of field secondary math teachers
+
+mod = PanelOLS.from_formula(
+    "outoffield_secondary_math_teacher" + EVENT_STUDY_MODEL, data=df
+)
+res = mod.fit(cov_type="clustered", clusters=df.district)
+print(res)
+
+# %%
+mod = PanelOLS.from_formula(
+    "outoffield_secondary_science_teacher" + EVENT_STUDY_MODEL, data=df
+)
+res = mod.fit(cov_type="clustered", clusters=df.district)
+print(res)
 # %%
