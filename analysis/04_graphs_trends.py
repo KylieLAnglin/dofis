37f03e633c87f754e7e1d04edf9bf03285a9e724
KylieLAnglin <kylie@anglin.com>
2021-02-11 08:59:10 -0500

    add all groups to trends graph

--- analysis/04_graphs_trends.py
+++ analysis/04_graphs_trends.py
@@ -198,3 +198,77 @@ for outcome in title_labels:
     fig.savefig(
         start.table_path + "trends_by_adoption_" + outcome + ".png", bbox_inches="tight"
     )
+
+# %%
+data = pd.read_csv(
+    os.path.join(start.data_path, "clean", "master_data_school.csv"),
+    sep=",",
+    low_memory=False,
+)
+
+outcome = "math"
+df_treat2017 = create_group_df(data[data.doi_year == 2017], outcome=outcome)
+df_treat2018 = create_group_df(data[data.doi_year == 2018], outcome=outcome)
+df_treat2019 = create_group_df(data[data.doi_year == 2019], outcome=outcome)
+df_control = create_group_df(data[data.doi == False], outcome=outcome)
+df_charter = create_group_df(data[data.distischarter == False], outcome=outcome)
+
+
+fig, ax = plt.subplots(1, 1)
+
+ax.plot(
+    list(df_treat2017.index),
+    df_treat2017["outcome"]["score_mean"],
+    label="2016-17 DOI Implementers",
+    linestyle="-",
+    color="black",
+)
+ax.plot(
+    list(df_treat2018.index),
+    df_treat2018["outcome"]["score_mean"],
+    label="2017-18 DOI Implementers",
+    linestyle="--",
+    color="black",
+)
+ax.plot(
+    list(df_treat2019.index),
+    df_treat2019["outcome"]["score_mean"],
+    label="2018-19 DOI Implementers",
+    linestyle=":",
+    color="black",
+)
+
+ax.plot(
+    list(df_control.index),
+    df_control["outcome"]["score_mean"],
+    label="Traditional Public School Districts",
+    linestyle="-.",
+    color="gray",
+)
+
+ax.plot(
+    list(df_charter.index),
+    df_charter["outcome"]["score_mean"],
+    label="Charter School Districts",
+    linestyle=":",
+    color="gray",
+)
+
+ax.legend()
+# ax.fill_between(list(df_treat2017.index), df_treat2017.lb, df_treat2017.ub, alpha=0.2)
+# ax.fill_between(list(df_treat2018.index), df_treat2018.lb, df_treat2018.ub, alpha=0.2)
+# ax.fill_between(list(df_treat2019.index), df_treat2019.lb, df_treat2019.ub, alpha=0.2)
+# ax.fill_between(list(df_control.index), df_control.lb, df_control.ub, alpha=0.2)
+
+
+ax.axvline(x=2016.5, linestyle="-", color="black")
+ax.axvline(x=2017.5, linestyle="--", color="black")
+ax.axvline(x=2018.5, linestyle=":", color="black")
+
+ax.set_title(title_labels[outcome])
+ax.grid(False)
+
+# fig.savefig(
+#     start.table_path + "trends_by_adoption_" + outcome + ".png", bbox_inches="tight"
+# )
+# %%
