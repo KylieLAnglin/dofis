53b711837a2756e05fa2b697a1f40245647bd7fa
KylieLAnglin <Kylie.Anglin@uconn.edu>
2022-01-11 10:48:29 -0500

    update home on start.R

--- r_scripts/00_start.R
+++ r_scripts/00_start.R
@@ -1,5 +1,6 @@
-output_path = "/Users/kylieanglin/Dropbox/Active/dofis/results/"
-data_path = "/Users/kylieanglin/Dropbox/Active/dofis/data/"
+home_path = "/Users/kla21002/Dropbox/Active/dofis/"
+output_path = paste(home_path, "results/", sep="")
+data_path = paste(home_path,"data/", sep = "")
 df <- read.csv(paste(data_path, "clean/r_data_school_2020_comparison.csv", sep=""))
 
 set.seed(42)
