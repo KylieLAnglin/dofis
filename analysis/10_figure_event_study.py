23af23a3cbff49b498127a8a6ceaa16c137ee3ea
KylieLAnglin <Kylie.Anglin@uconn.edu>
2022-01-11 15:33:24 -0500

    working event study graph

--- analysis/10_figure_event_study.py
+++ analysis/10_figure_event_study.py
@@ -1,23 +1,47 @@
 # %% Event Study Graphs - Math
+# %%
+# %%
+import pandas as pd
+import numpy as np
+import pandas as pd
+from openpyxl import load_workbook
+import scipy
+import matplotlib.pyplot as plt
 
-mod = PanelOLS.from_formula("math" + EVENT_STUDY_MODEL, df)
-res = mod.fit(cov_type="clustered", clusters=df.district)
+from dofis import start
+from dofis.analysis.library import analysis
 
-nonparametric = []
-nonparametric_se = []
-for coef in ["pre5", "pre4", "pre3", "pre2", "pre1", "post1", "post2", "post3"]:
-    nonpar = 0
-    nonpar_se = 0
-    if coef != "pre1":
-        nonpar = res.params[coef]
-        nonpar_se = res.std_errors[coef]
-    nonparametric.append(nonpar)
-    nonparametric_se.append(nonpar_se)
+MATH_AGG = start.TABLE_PATH + "results_math_ag_raw.xlsx"
+math_agg = pd.read_excel(MATH_AGG)
+
+MATH_DISAG = start.TABLE_PATH + "results_math_disag_raw.xlsx"
+math_disag = pd.read_excel(MATH_DISAG)
+
+READING_AGG = start.TABLE_PATH + "results_reading_ag_raw.xlsx"
+reading_agg = pd.read_excel(READING_AGG)
+
+READING_DISAG = start.TABLE_PATH + "results_reading_disag_raw.xlsx"
+reading_disag = pd.read_excel(READING_DISAG)
+
+data = pd.read_csv(start.DATA_PATH + "clean/r_data_school_2020_comparison.csv")
+n = data.district.nunique()
+
+# %%
+# %%
+coefs = []
+ses = []
+for row in [0, 1, 2, 3, 4, 5, 6, 7, 8]:
+    coef = math_agg.loc[row]["agg.dynamic.math.att.egt"]
+    se = math_agg.loc[row]["agg.dynamic.math.se.egt"]
+    coefs.append(coef)
+    ses.append(se)
+
+
 coef_df = pd.DataFrame(
     {
-        "coef": nonparametric,
-        "err": nonparametric_se,
-        "year": [-5, -4, -3, -2, -1, 1, 2, 3],
+        "coef": coefs,
+        "err": ses,
+        "year": [-6, -5, -4, -3, -2, -1, 1, 2, 3],
     }
 )
 coef_df["lb"] = coef_df.coef - (1.96 * coef_df.err)
@@ -41,9 +65,12 @@ ax.scatter(
 ax.axhline(y=0, linestyle="--", color="black", linewidth=4)
 ax.xaxis.set_ticks_position("none")
 _ = ax.set_xticklabels(
-    ["Pre5", "Pre4", "Pre3", "Pre2", "Pre1", "Post1", "Post2", "Post3"], rotation=0
+    ["Pre6", "Pre5", "Pre4", "Pre3", "Pre2", "Pre1", "Post1", "Post2", "Post3"],
+    rotation=0,
 )
 # ax.set_title('Impact on Student Achievement - Event Study Coefficients',
 # fontsize = 16)
 
 # fig.savefig(start.table_path + "math_event_study" + ".png", bbox_inches="tight")
+
+# %%
