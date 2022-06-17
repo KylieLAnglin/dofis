f4b05e86a0fa1b9fd5779d3e4b1cf87fa4de0514
KylieLAnglin <Kylie.Anglin@uconn.edu>
2022-06-17 10:15:59 -0400

    add new year to teachers (more cleaning needed)

--- data_from_tea/t02_clean_teachers.py
+++ data_from_tea/t02_clean_teachers.py
@@ -7,13 +7,30 @@ from dofis.data_from_tea.library import clean_tea
 from dofis.data_from_tea.library import build
 
 year = "yr1718"
-for year in ["yr1213", "yr1314", "yr1415", "yr1516", "yr1617", "yr1718", "yr1819"]:
+for year in [
+    "yr1213",
+    "yr1314",
+    "yr1415",
+    "yr1516",
+    "yr1617",
+    "yr1718",
+    "yr1819",
+    "yr1920",
+    "yr2021",
+    "yr2122",
+]:
     # Read files
     teacher_datapath = os.path.join(start.DATA_PATH, "teachers", year)
     pattern = "TEACHER_MASTER*.TXT"
 
+    if year > "yr1819":
+        pattern = "*TEACHER_MASTER.csv"
+
     teachers = build.concat_files(path=teacher_datapath, pattern=pattern)
-    teachers = teachers[teachers["ROLE NAME"] == "TEACHER"]
+    if year < "yr1920":
+        teachers = teachers[teachers["ROLE NAME"] == "TEACHER"]
+    else:
+        teachers = teachers[teachers["ROLEX"] == "TEACHER"]
 
     vars_to_keep = {
         "SCRAMBLED UNIQUE ID": "teacher_id",
@@ -25,6 +42,18 @@ for year in ["yr1213", "yr1314", "yr1415", "yr1516", "yr1617", "yr1718", "yr1819
         "ROLE FULL TIME EQUIVALENT": "fte_teacher",
         "EXPERIENCE": "experience",
     }
+    if year > "yr1819":
+        vars_to_keep = {
+            "PERSONID_SCRAM": "teacher_id",
+            "DISTRICT": "district",
+            "DISTNAME": "distname",
+            "CAMPUS": "campus",
+            "CAMPNAME": "campname",
+            "GRADEGRP1X": "camp_grade_group",
+            "PFTE": "fte_teacher",
+            # "EXPERIENCE": "experience",
+            #  "TOTALPAY": "pay",
+        }
     teachers = clean_tea.filter_and_rename_cols(teachers, vars_to_keep)
 
     teachers.sort_values(by=["teacher_id"], axis=0)
