b3bec4ab23abc00268f84dad5ace7865eca56f07
KylieLAnglin <Kylie.Anglin@uconn.edu>
2022-06-17 09:58:04 -0400

    create table describing implementation groups

--- analysis/table_district_characteristics_by_timing.py
+++ analysis/table_district_characteristics_by_timing.py
@@ -10,6 +10,9 @@ import sys
 import numpy as np
 import pandas as pd
 import statsmodels.formula.api as smf
+import scipy
+from openpyxl import load_workbook
+
 
 from dofis.analysis.library import analysis, characteristics, regulations, tables
 from dofis import start
@@ -23,58 +26,163 @@ data = pd.read_csv(
     os.path.join(start.DATA_PATH, "clean", "master_data_district.csv"), sep=","
 )
 data = data[data.year == 2016]
-data = data[(data.doi == 1)]
 data["teachers_turnover_ratio_d"] = data.teachers_turnover_ratio_d / 100
+data.group.value_counts()
+
+print("Number ineligble districts:", len(data[data.group == "ineligible"]))
+data[data.group != "ineligible"]
+print(
+    "Number DOIs with missing implementation year:",
+    len(data[(data.doi == 1) & (data.doi_year.isnull())]),
+)
 
 # %%
+# %%
+FILE = start.TABLE_PATH + "Characteristics of Implementation Groups.xlsx"
+wb = load_workbook(FILE)
+ws = wb.active
 
-for reg, col in zip(
-    [
-        "reg25_0811",
-        "reg25_081",
-        "reg25_0812",
-        "reg25_082",
-        "reg25_112",
-        "reg25_113",
-        "reg21_003",
-        "reg21_053",
-        "reg21_057",
-        "reg21_102",
-        "reg21_401",
-        "reg21_352",
-        "reg25_092",
-        "reg37_0012",
-        "reg25_036",
-    ],
-    list(range(2, 17)),
+
+# %% Sample sizes
+ws.cell(row=3, column=2).value = "N = " + str(len(data[data.group == "opt-out"]))
+ws.cell(row=3, column=3).value = "N = " + str(len(data[data.group == "2017"]))
+ws.cell(row=3, column=4).value = "N = " + str(len(data[data.group == "2018"]))
+ws.cell(row=3, column=5).value = "N = " + str(len(data[data.group == "2019"]))
+ws.cell(row=3, column=6).value = "N = " + str(len(data[data.group == "2020"]))
+
+# %%
+df = data[data.group == "opt-out"]
+
+
+def mean_and_sd_to_table(
+    df: pd.DataFrame, variable: str, start_row: int, col: int, round_value: int = 2
 ):
-    teacher = analysis.many_y_one_x(
-        data=data,
-        y_list=characteristics.teacher,
-        y_labels=characteristics.labels,
-        x=reg,
+    ws.cell(row=start_row, column=col).value = str(
+        round(df[variable].mean(), round_value)
     )
-    student = analysis.many_y_one_x(
-        data=data,
-        y_list=characteristics.student,
-        y_labels=characteristics.labels,
-        x=reg,
+    # ws.cell(row=start_row + 1, column=col).value = (
+    #     "[" + str(round(df[variable].std(), round_value)) + "]"
+    # )
+    ws.cell(row=start_row + 1, column=col).value = (
+        "("
+        + str(round(scipy.stats.sem(df[variable], nan_policy="omit"), round_value))
+        + ")"
     )
-    dfs = [teacher, student]
-    rows = [7, 16]
-    for df, row in zip(dfs, rows):
-        tables.just_diff_to_excel(
-            file=start.TABLE_PATH + "District Characteristics by Exemption.xlsx",
-            df=df,
-            diff_col="Difference",
-            se_col="Std. Error",
-            pvalue_col="P-value",
-            start_col=col,
-            start_row=row,
-        )
-    tables.n_to_excel(
-        file=start.TABLE_PATH + "District Characteristics by Exemption.xlsx",
+
+
+for group, col in zip(
+    [
+        "2017",
+        "2018",
+        "2019",
+        "2020",
+        "opt-out",
+    ],
+    [2, 3, 4, 5, 6],
+):
+    row = 4
+    mean_and_sd_to_table(
+        df=data[data.group == group], variable="type_urban", start_row=row, col=col
+    )
+    row = row + 2
+
+    mean_and_sd_to_table(
+        df=data[data.group == group], variable="type_suburban", start_row=row, col=col
+    )
+    row = row + 2
+
+    mean_and_sd_to_table(
+        df=data[data.group == group], variable="type_town", start_row=row, col=col
+    )
+    row = row + 2
+
+    mean_and_sd_to_table(
+        df=data[data.group == group], variable="type_rural", start_row=row, col=col
+    )
+    row = row + 2
+
+    mean_and_sd_to_table(
+        df=data[data.group == group],
+        variable="teachers_exp_ave",
+        start_row=row,
         col=col,
-        row=5,
-        n=data[data[reg] == 1].district.nunique(),
+        round_value=1,
     )
+    row = row + 2
+
+    mean_and_sd_to_table(
+        df=data[data.group == group],
+        variable="teachers_tenure_ave",
+        start_row=row,
+        col=col,
+        round_value=1,
+    )
+    row = row + 2
+
+    mean_and_sd_to_table(
+        df=data[data.group == group],
+        variable="teachers_turnover_ratio_d",
+        start_row=row,
+        col=col,
+        round_value=2,
+    )
+    row = row + 2
+
+    mean_and_sd_to_table(
+        df=data[data.group == group],
+        variable="class_size_5",
+        start_row=row,
+        col=col,
+        round_value=1,
+    )
+    row = row + 2
+
+    mean_and_sd_to_table(
+        df=data[data.group == group],
+        variable="students_hisp",
+        start_row=row,
+        col=col,
+        round_value=2,
+    )
+    row = row + 2
+
+    mean_and_sd_to_table(
+        df=data[data.group == group],
+        variable="students_black",
+        start_row=row,
+        col=col,
+        round_value=2,
+    )
+    row = row + 2
+
+    mean_and_sd_to_table(
+        df=data[data.group == group],
+        variable="students_white",
+        start_row=row,
+        col=col,
+        round_value=2,
+    )
+    row = row + 2
+
+    mean_and_sd_to_table(
+        df=data[data.group == group],
+        variable="students_frpl",
+        start_row=row,
+        col=col,
+        round_value=2,
+    )
+    row = row + 2
+
+    mean_and_sd_to_table(
+        df=data[data.group == group],
+        variable="avescores",
+        start_row=row,
+        col=col,
+        round_value=2,
+    )
+    row = row + 2
+
+
+wb.save(FILE)
+
+# %%
