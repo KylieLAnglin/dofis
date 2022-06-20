d1fa8573f1cdb9e7ae61fe22d5edc3731a103da4
KylieLAnglin <Kylie.Anglin@uconn.edu>
2022-06-20 14:37:05 -0500

    Add control and 2020+ to trends graphs

--- analysis/figures_trends.py
+++ analysis/figures_trends.py
@@ -24,10 +24,7 @@ plt.figure(figsize=(480 / my_dpi, 480 / my_dpi), dpi=my_dpi)
 
 # %%
 
-data = pd.read_csv(
-    os.path.join(start.DATA_PATH, "clean", "gdid_school.csv"), sep=",", low_memory=False
-)
-data[data.year == 2019].doi_year.value_counts().sort_index()
+data = pd.read_csv(start.DATA_PATH + "clean/master_data_school.csv")
 
 # %% Visual Impact by Subject
 
@@ -64,6 +61,8 @@ title_labels = {
     "teachers_secondary_math_outoffield": "Proportion Out of Field Secondary Math Teachers",
     "teachers_secondary_science_outoffield": "Proportion Out of Field Secondary Science Teachers",
     "teachers_secondary_cte_outoffield": "Proportion Out of Field Secondary CTE Teachers",
+    "teachers_exp_ave": "Average Teacher Experience",
+    "teachers_num": "Number of Teachers",
     "days_mean": "District Average Days",
     "perf_attendance": "Student Attendance",
     "perf_stu_days": "Student Days",
@@ -71,11 +70,14 @@ title_labels = {
 
 
 for outcome in title_labels:
-    df_treat2017 = create_group_df(data[data.doi_year == 2017], outcome=outcome)
-    df_treat2018 = create_group_df(data[data.doi_year == 2018], outcome=outcome)
-    df_treat2019 = create_group_df(data[data.doi_year == 2019], outcome=outcome)
-    df_treat2020 = create_group_df(data[data.doi_year == 2020], outcome=outcome)
-
+    df_treat2017 = create_group_df(data[data.group == "2017"], outcome=outcome)
+    df_treat2018 = create_group_df(data[data.group == "2018"], outcome=outcome)
+    df_treat2019 = create_group_df(data[data.group == "2019"], outcome=outcome)
+    df_treat2020 = create_group_df(
+        data[data.group == "2020+"],
+        outcome=outcome,
+    )
+    df_control = create_group_df(data[data.group == "opt-out"], outcome=outcome)
     monochrome = cycler("color", ["k"]) * cycler("linestyle", ["-", "--", ":", "-."])
 
     fig, ax = plt.subplots(1, 1)
@@ -85,22 +87,38 @@ for outcome in title_labels:
         list(df_treat2017.index),
         df_treat2017["outcome"]["score_mean"],
         label="2016-17 DOI Implementers",
+        color="gray",
+        linestyle="dotted",
     )
     ax.plot(
         list(df_treat2018.index),
         df_treat2018["outcome"]["score_mean"],
         label="2017-18 DOI Implementers",
+        color="gray",
+        linestyle="solid",
     )
     ax.plot(
         list(df_treat2019.index),
         df_treat2019["outcome"]["score_mean"],
         label="2018-19 DOI Implementers",
+        color="black",
+        linestyle="dotted",
     )
 
     ax.plot(
         list(df_treat2020.index),
         df_treat2020["outcome"]["score_mean"],
-        label="2019-20 DOI Implementers",
+        label="2019-20 to 2020-21 DOI Implementers",
+        color="black",
+        linestyle="dashed",
+    )
+
+    ax.plot(
+        list(df_control.index),
+        df_control["outcome"]["score_mean"],
+        label="Districts Opting Out",
+        color="black",
+        linestyle="solid",
     )
 
     ax.legend()
@@ -118,130 +136,13 @@ for outcome in title_labels:
         list(df_treat2020.index), df_treat2020.lb, df_treat2020.ub, alpha=0.2
     )
 
-    ax.axvline(x=2016.5, linestyle="-", color="black")
-    ax.axvline(x=2017.5, linestyle="--", color="black")
-    ax.axvline(x=2018.5, linestyle=":", color="black")
+    ax.fill_between(list(df_control.index), df_control.lb, df_control.ub, alpha=0.2)
 
     ax.set_title(title_labels[outcome])
     ax.grid(False)
 
     fig.savefig(
-        start.TABLE_PATH + "trends_by_adoption_" + outcome + ".png", bbox_inches="tight"
+        start.TABLE_PATH + "trends_by_adoption_" + outcome + ".pdf", bbox_inches="tight"
     )
 
 # %%
-data = pd.read_csv(
-    os.path.join(start.DATA_PATH, "clean", "master_data_school.csv"),
-    sep=",",
-    low_memory=False,
-)
-
-outcome = "teachers_uncertified"
-df_treat2017 = create_group_df(data[data.doi_year == 2017], outcome=outcome)
-df_treat2018 = create_group_df(data[data.doi_year == 2018], outcome=outcome)
-df_treat2019 = create_group_df(data[data.doi_year == 2019], outcome=outcome)
-df_treat2020 = create_group_df(data[data.doi_year == 2020], outcome=outcome)
-df_control = create_group_df(data[data.doi == False], outcome=outcome)
-df_charter = create_group_df(data[data.distischarter == False], outcome=outcome)
-
-
-fig, ax = plt.subplots(1, 1)
-
-ax.plot(
-    list(df_treat2017.index),
-    df_treat2017["outcome"]["score_mean"],
-    label="2016-17 DOI Implementers",
-    linestyle="-",
-    color="black",
-)
-ax.plot(
-    list(df_treat2018.index),
-    df_treat2018["outcome"]["score_mean"],
-    label="2017-18 DOI Implementers",
-    linestyle="--",
-    color="black",
-)
-ax.plot(
-    list(df_treat2019.index),
-    df_treat2019["outcome"]["score_mean"],
-    label="2018-19 DOI Implementers",
-    linestyle=":",
-    color="black",
-)
-
-ax.plot(
-    list(df_treat2020.index),
-    df_treat2020["outcome"]["score_mean"],
-    label="2020 Implementers",
-    linestyle="-.",
-    color="gray",
-)
-
-ax.plot(
-    list(df_control.index),
-    df_control["outcome"]["score_mean"],
-    label="Non-DOIs",
-    linestyle="solid",
-    color="gray",
-)
-
-# ax.plot(
-#     list(df_charter.index),
-#     df_charter["outcome"]["score_mean"],
-#     label="Charter School Districts",
-#     linestyle=":",
-#     color="gray",
-# )
-
-ax.legend()
-# ax.fill_between(list(df_treat2017.index), df_treat2017.lb, df_treat2017.ub, alpha=0.2)
-# ax.fill_between(list(df_treat2018.index), df_treat2018.lb, df_treat2018.ub, alpha=0.2)
-# ax.fill_between(list(df_treat2019.index), df_treat2019.lb, df_treat2019.ub, alpha=0.2)
-# ax.fill_between(list(df_control.index), df_control.lb, df_control.ub, alpha=0.2)
-
-
-ax.axvline(x=2016.5, linestyle="-", color="black")
-ax.axvline(x=2017.5, linestyle="--", color="black")
-ax.axvline(x=2018.5, linestyle=":", color="black")
-ax.set_ylim(0, 0.02)
-ax.set_title(title_labels[outcome])
-ax.grid(False)
-
-# fig.savefig(
-#     start.TABLE_PATH + "trends_by_adoption_" + outcome + ".png", bbox_inches="tight"
-# )
-# # %%
-# outcome = "math"
-# df_treat = create_group_df(data[data.doi == True], outcome=outcome)
-# df_control = create_group_df(data[data.doi == False], outcome=outcome)
-# df_charter = create_group_df(data[data.distischarter == False], outcome=outcome)
-
-
-# fig, ax = plt.subplots(1, 1)
-
-# ax.plot(
-#     list(df_treat.index),
-#     df_treat["outcome"]["score_mean"],
-#     label="DOIs",
-#     linestyle="-",
-#     color="black",
-# )
-# ax.plot(
-#     list(df_charter.index),
-#     df_charter["outcome"]["score_mean"],
-#     label="Charter Schools",
-#     linestyle="--",
-#     color="black",
-# )
-# ax.axvline(x=2016, linestyle="-", color="black")
-
-
-# ax.legend()
-
-
-# ax.set_title(title_labels[outcome])
-# ax.grid(False)
-
-
-# # %%
-
