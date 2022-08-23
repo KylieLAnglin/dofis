b0e8dfa60d388f9b3b6b3d62cbba6119c8c70a70
KylieLAnglin <Kylie.Anglin@uconn.edu>
2022-08-23 11:41:29 -0400

    create indicators for cert desc with "any"

--- clean_certification_database.py
+++ clean_certification_database.py
@@ -1,5 +1,6 @@
 # %%
 import pandas as pd
+import numpy as np
 from dofis import start
 
 cert_licences = pd.read_excel(
@@ -18,11 +19,83 @@ course_requirements = course_requirements.rename(
 )
 course_requirements = course_requirements[["cert_id", "class_number"]]
 
+
 # %%
 course_cert = course_requirements.merge(
     cert_licences, how="left", on=["cert_id"], indicator=True
 )
 
+
+# %%
+course_cert["cert_any_desc"] = np.where(
+    (course_cert.cert_desc.str.contains("Any "))
+    | course_cert.cert_desc.str.contains("any "),
+    1,
+    0,
+)
+course_cert["any_trade"] = np.where(
+    course_cert.cert_desc
+    == "Any Trades and Industrial Education or Vocational Trades and Industry certificate",
+    1,
+    0,
+)
+course_cert["any_elem"] = np.where(
+    course_cert.cert_desc == "Any elementary certificate", 1, 0
+)
+course_cert["any_secondary"] = np.where(
+    course_cert.cert_desc == "Any secondary certificate", 1, 0
+)
+course_cert["any_all_level"] = np.where(
+    course_cert.cert_desc == "Any all-level certificate", 1, 0
+)
+course_cert["any_school_decision"] = np.where(
+    course_cert.cert_desc.str.startswith(
+        "Any teacher certificate appropriate for grade level of assignment or appropriate qualifications as determined by the school"
+    ),
+    1,
+    0,
+)
+course_cert["any_language_grade_match"] = np.where(
+    course_cert.cert_desc
+    == "Any teacher certificate in the appropriate language and grade level of assignment",
+    1,
+    0,
+)
+course_cert["any_cte"] = np.where(
+    (
+        course_cert.cert_desc
+        == "Any vocational or career and technical education (CTE) classroom teaching certificate"
+    )
+    | (
+        course_cert.cert_desc
+        == "Any vocational or career and technical education (CTE) classroom teaching certificate with a bachelor’s degree and 18 semester credit hours in any combination of sciences"
+    ),
+    1,
+    0,
+)
+course_cert["any_subject_grade_match"] = np.where(
+    course_cert.cert_desc
+    == "Any valid classroom teaching certificate appropriate for the grade level and subject area",
+    1,
+    0,
+)
+course_cert["any_grade_match"] = np.where(
+    course_cert.cert_desc
+    == "Any teacher certificate appropriate for grade level of assignment",
+    1,
+    0,
+)
+
+course_cert["any_cert"] = np.where(
+    course_cert.cert_desc == "Any valid classroom teacher or administrator certificate",
+    1,
+    0,
+)
+course_cert["any_esl"] = np.where(course_cert.cert_desc == "Any ESL certificate", 1, 0)
+course_cert["any_sped"] = np.where(
+    course_cert.cert_desc == "Any Special Education certificate", 1, 0
+)
+# %%
 course_cert.to_csv(
     "/Users/kla21002/Dropbox/Active/dofis/data/teachers/field_requirements.csv",
     index=False,
