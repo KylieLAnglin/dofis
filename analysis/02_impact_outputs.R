86a40c1b1d9f3d09b4bc1821e94ef8e1b55ccdf6
KylieLAnglin <Kylie.Anglin@uconn.edu>
2022-09-16 13:38:44 -0400

    delete unused analyses

--- analysis/02_impact_outputs.R
+++
@@ -1,3 +0,0 @@
-# 
-df <- read.csv(paste(data_path, "clean/r_data.csv", sep=""))
-run_and_export_main(df = df, outcome = "math_yr15std", disag_file = "results_temp_disag_raw.xlsx", ag_file = "results_temp_ag_raw.xlsx")
